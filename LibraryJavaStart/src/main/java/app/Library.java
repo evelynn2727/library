4a955747ed571cb6c82cdb5077d9446f52130729
Evelyn <totalnieniewybaczalne.ep@gmail.com>
2019-02-26 01:24:28 +0100

    DataReader class

--- LibraryJavaStart/src/main/java/app/Library.java
+++ LibraryJavaStart/src/main/java/app/Library.java
@@ -1,6 +1,7 @@
 package app;
 
 import data.Book;
+import utils.DataReader;
 
 public class Library {
     public static void main(String[] args){
@@ -11,12 +12,16 @@ public class Library {
         books[1] = new Book("Java. Efektywne programowanie. Wydanie II", "Josh" ,  2010,352, "Helion", "9788324620845");
         books[2] = new Book("SCJP Sun Certied Programmer for Java 6",   "Bert Bates, Katherine Sierra", 2008, 851, "McGraw-Hill Osborne Media","9780071591065");
 
+        System.out.println("wprowadz nowe ksiazki: ");
+        DataReader dr= new DataReader();
+        books[3]=dr.readAndCreateBook();
         System.out.print(appName);
         System.out.println("Książki dostępne w bibliotece:");
-        for (Book book:books
-             ) {
-            book.printInfo();
-        }
+//        for (Book booko:books
+//             ) {
+//            booko.printInfo();
+//        }
         System.out.println("System może przechowywać do "+books.length+" ksiażek");
+
     }
 }
