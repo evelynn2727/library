f27704f5ce9d04f014d8b4fb0f91dd38024870fe
Evelyn <totalnieniewybaczalne.ep@gmail.com>
2019-03-16 23:05:41 +0100

    locale class in LibraryControl with enum

--- LibraryJavaStart/src/main/java/app/LibraryControl.java
+++ LibraryJavaStart/src/main/java/app/LibraryControl.java
@@ -78,4 +78,40 @@ public class LibraryControl {
     {
         LibraryUtils.printMagazines(library);
     }
+
+    public enum Option {
+        EXIT(0, "Wyjscie z programu"),
+        ADD_BOOK(1,"Dodanie ksiazki"),
+        ADD_MAGAZINES(2, "Dodanie magazynu/gazety"),
+        PRINT_BOOKS(3, "Wyswietlanie dostepych ksiazek"),
+        PRINT_MAGAZINES(4,"Wyswietlanie dostepnych magazynow/gazet");
+
+        //przechowuje informacje o dwoch wartosciach
+        private int value;//przechowuje mozliwa do wprowadzenia przez uzytkownika wartosc
+        private String description;//opis tego za co odpowiada dana wartosc
+
+        Option(int value, String description) {
+            this.value = value;
+            this.description = description;
+        }
+
+        @Override
+        public String toString() {
+            return  value + " - " + description;
+        }
+
+        //pozwala przeksztalcic wartosc typi int na odpowiednia wartosc typu Option
+        //metoda ta dzieki values() zwraca tablice wszystkich wartosic, a poniewaz kolejne elementy numerujemy od 0, to zwracamy po prostu odpowiedni element tej tablicy
+        public static Option createFromInt(int option) throws NoSuchElementException {
+            Option result=null;
+            try{
+                result=Option.values()[option];
+            }catch (ArrayIndexOutOfBoundsException e){
+                //thr - throw new +Tab
+                throw new NoSuchElementException("Brak elementu o wskazanym id");
+            }
+            return result;
+        }
+    }
+
 }
