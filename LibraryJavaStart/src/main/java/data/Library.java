cbe867ea00589446ded39ac13d518b922f19b5de
Evelyn <totalnieniewybaczalne.ep@gmail.com>
2019-03-29 16:04:56 +0100

    remove publication function

--- LibraryJavaStart/src/main/java/data/Library.java
+++ LibraryJavaStart/src/main/java/data/Library.java
@@ -1,16 +1,13 @@
 package data;
 
 import java.io.Serializable;
+import java.util.Arrays;
 
 public class Library implements Serializable {
     private static final long serialVersionUID = 2995794334600947814L;
-    public static final int MAX_PUBLICATIONS=2000; //maksymalna liczba ksiazek I MAGAZYNOW
     private Publication[] publications;
     private int publicationsNumber;
-
-    public static int getMaxPublications() {
-        return MAX_PUBLICATIONS;
-    }
+    public static final int INITIAL_CAPACITY=1;
 
     public Publication[] getPublications() {
         return publications;
@@ -29,17 +26,41 @@ public class Library implements Serializable {
     }
 
     public Library() {
-        publications= new Publication[MAX_PUBLICATIONS];
+        publications= new Publication[INITIAL_CAPACITY];
     }
 
     public void addPublications(Publication pub){
-        if(publicationsNumber==MAX_PUBLICATIONS){
-            throw new ArrayIndexOutOfBoundsException("skonczylo sie miejsce");
-        }
+        if(publicationsNumber==publications.length){
+            publications= Arrays.copyOf(publications,publications.length*2);
+              }
         publications[publicationsNumber]=pub;
         publicationsNumber++;
     }
 
+    public void removePublication(Publication pub){
+        // remove() pozwala na usunięcie obiektu Publication z tablicy jednocześnie nie
+        //pozostawiając w niej pustych miejsc, ponieważ wszystkie elementy, które znajdowały się "na
+        //prawo" od usuniętego elementu zostają dosunięte w puste miejsce za pomocą metody
+        //System.arraycopy()
+        if(pub==null){
+            return;
+        }
+
+        final int NOT_FOUND=-1;
+        int found= NOT_FOUND;
+        int i=0;
+        while (i<publications.length && found==NOT_FOUND){
+            if (pub.equals(publications[i])){
+                found=i;
+            }else{
+                i++;
+            }
+        }
+        if(found!=NOT_FOUND){
+            System.arraycopy(publications, found+1,publications,found, publications.length);
+            publicationsNumber--;
+        }
+    }
 
     public void addMagazines(Magazine magazine){
      addPublications(magazine);
